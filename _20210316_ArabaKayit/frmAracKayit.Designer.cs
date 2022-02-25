e6a363444d8665c75c227d28588c04b324886e33
budurmete <budurmete@gmail.com>
2022-02-25 20:53:18 +0300

    Proje dosyası ekle.

---
+++ _20210316_ArabaKayit/frmAracKayit.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace _20210316_ArabaKayit
+{
+    partial class frmAracKayit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtMarka = new System.Windows.Forms.TextBox();
+            this.cmbVites = new System.Windows.Forms.ComboBox();
+            this.btnRenkSec = new System.Windows.Forms.Button();
+            this.txtPlaka = new System.Windows.Forms.TextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.listArac = new System.Windows.Forms.ListView();
+            this.numDaireNo = new System.Windows.Forms.NumericUpDown();
+            this.dialogRenkSec = new System.Windows.Forms.ColorDialog();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.numDaireNo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(106, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(85, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(69, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Daire No";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(367, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Marka/Model";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(417, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Vites";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(425, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Renk";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(422, 159);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Plaka";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAd.Location = new System.Drawing.Point(157, 51);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(186, 26);
+            this.txtAd.TabIndex = 8;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSoyad.Location = new System.Drawing.Point(157, 81);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(186, 26);
+            this.txtSoyad.TabIndex = 9;
+            // 
+            // txtMarka
+            // 
+            this.txtMarka.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMarka.Location = new System.Drawing.Point(480, 48);
+            this.txtMarka.Name = "txtMarka";
+            this.txtMarka.Size = new System.Drawing.Size(186, 26);
+            this.txtMarka.TabIndex = 11;
+            // 
+            // cmbVites
+            // 
+            this.cmbVites.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbVites.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbVites.FormattingEnabled = true;
+            this.cmbVites.Items.AddRange(new object[] {
+            "Manual",
+            "Yarı Otomatik",
+            "Otomatik"});
+            this.cmbVites.Location = new System.Drawing.Point(480, 80);
+            this.cmbVites.Name = "cmbVites";
+            this.cmbVites.Size = new System.Drawing.Size(186, 28);
+            this.cmbVites.TabIndex = 12;
+            // 
+            // btnRenkSec
+            // 
+            this.btnRenkSec.Location = new System.Drawing.Point(480, 115);
+            this.btnRenkSec.Name = "btnRenkSec";
+            this.btnRenkSec.Size = new System.Drawing.Size(186, 32);
+            this.btnRenkSec.TabIndex = 13;
+            this.btnRenkSec.Text = "Renk Seç";
+            this.btnRenkSec.UseVisualStyleBackColor = true;
+            this.btnRenkSec.Click += new System.EventHandler(this.btnRenkSec_Click);
+            // 
+            // txtPlaka
+            // 
+            this.txtPlaka.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPlaka.Location = new System.Drawing.Point(480, 156);
+            this.txtPlaka.Name = "txtPlaka";
+            this.txtPlaka.Size = new System.Drawing.Size(186, 26);
+            this.txtPlaka.TabIndex = 14;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btnKaydet.Location = new System.Drawing.Point(692, 45);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(186, 34);
+            this.btnKaydet.TabIndex = 15;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // listArac
+            // 
+            this.listArac.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.listArac.FullRowSelect = true;
+            this.listArac.GridLines = true;
+            this.listArac.HideSelection = false;
+            this.listArac.Location = new System.Drawing.Point(89, 224);
+            this.listArac.Name = "listArac";
+            this.listArac.Size = new System.Drawing.Size(824, 214);
+            this.listArac.TabIndex = 16;
+            this.listArac.UseCompatibleStateImageBehavior = false;
+            this.listArac.View = System.Windows.Forms.View.Details;
+            this.listArac.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listArac_KeyDown);
+            // 
+            // numDaireNo
+            // 
+            this.numDaireNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numDaireNo.Location = new System.Drawing.Point(157, 112);
+            this.numDaireNo.Name = "numDaireNo";
+            this.numDaireNo.Size = new System.Drawing.Size(86, 26);
+            this.numDaireNo.TabIndex = 17;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Ad";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Soyad";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Daire No";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Marka / Model";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Vites";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Renk";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Plaka";
+            // 
+            // frmAracKayit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 450);
+            this.Controls.Add(this.numDaireNo);
+            this.Controls.Add(this.listArac);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.txtPlaka);
+            this.Controls.Add(this.btnRenkSec);
+            this.Controls.Add(this.cmbVites);
+            this.Controls.Add(this.txtMarka);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmAracKayit";
+            this.Text = "Araç Kayıt";
+            ((System.ComponentModel.ISupportInitialize)(this.numDaireNo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtMarka;
+        private System.Windows.Forms.ComboBox cmbVites;
+        private System.Windows.Forms.Button btnRenkSec;
+        private System.Windows.Forms.TextBox txtPlaka;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.ListView listArac;
+        private System.Windows.Forms.NumericUpDown numDaireNo;
+        private System.Windows.Forms.ColorDialog dialogRenkSec;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+    }
+}
---
+++ _20210316_ArabaKayit/frmAracKayit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20210316_ArabaKayit
+{
+    public partial class frmAracKayit : Form
+    {
+        public frmAracKayit()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRenkSec_Click(object sender, EventArgs e)
+        {
+           DialogResult sonuc = dialogRenkSec.ShowDialog();
+           if(sonuc == DialogResult.OK)// kullanıcı Tamam demişse
+           {
+                btnRenkSec.BackColor = dialogRenkSec.Color;
+           }
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            ListViewItem satir = new ListViewItem();
+            // Text içindeki değer ilk sütuna atılır.
+            satir.Text = txtAd.Text;
+            satir.SubItems.Add(txtSoyad.Text);
+            satir.SubItems.Add(numDaireNo.Value.ToString());
+            satir.SubItems.Add(txtMarka.Text);
+            satir.SubItems.Add(cmbVites.Text);
+
+            satir.UseItemStyleForSubItems = false;
+            satir.SubItems.Add("");
+            satir.SubItems[5].BackColor = btnRenkSec.BackColor;
+
+            satir.SubItems.Add(txtPlaka.Text);
+
+            listArac.Items.Add(satir);
+        }
+
+        private void listArac_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                // Silmek istediğinize emin misiniz?
+                DialogResult sonuc = MessageBox.Show("Silmek istediğinize emin misiniz?", "Uyarı", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+
+                if (sonuc == DialogResult.Yes)
+                {
+                    listArac.Items.Remove(listArac.SelectedItems[0]);
+                }
+            }   
+        }
+    }
+}
